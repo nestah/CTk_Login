a95723087b0258c73c31c57f9f1a92ebee772770
Phestus <phestuskitsao254@gmail.com>
2023-12-08 17:37:50 +0300

    LoginGUI

---
+++ main.py
@@ -0,0 +1,32 @@
+import customtkinter
+
+customtkinter.set_appearance_mode("system") #appearance_mode options include: dark, light and system for default
+customtkinter.set_default_color_theme("green") #themes include: dark-blue, blue and green
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+# login function
+def login():
+    print("Trial")
+
+# ctk widgets
+frame = customtkinter.CTkFrame(master=root)    
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login System", font=('Roboto', 24))
+label.pack(pady=12, padx=10)
+
+entry1 = customtkinter.CTkEntry(master=frame, placeholder_text="Username")
+entry1.pack(pady=12, padx=10)
+
+entry2 = customtkinter.CTkEntry(master=frame, placeholder_text="Password", show="*")
+entry2.pack(pady=12, padx=10)
+
+button = customtkinter.CTkButton(master=frame, text="Login", command=login)
+button.pack(pady=12, padx=10)
+
+checkbox = customtkinter.CTkCheckBox(master=frame, text="Remember Me")
+checkbox.pack(pady=12, padx=10)
+
+root.mainloop()
